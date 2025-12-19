0c120c73e3c89665d5bc0c9932367f1461240d9b
Ei_chann <rifqi070409@gmail.com>
2025-12-19 22:34:33 +0700

    arghh

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -22,7 +22,9 @@ class AuthController extends Controller
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
 
-            return redirect('/dashboard');
+            // return redirect('/dashboard');
+            return redirect()->intended(route('dashboard'));
+
         }
 
         return back()->withErrors([
